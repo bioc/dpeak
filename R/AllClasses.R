bb767aef1948728906a175b1e4a31203fafccad1
carter-allen <carter.allen12@gmail.com>
2020-02-25 13:10:21 -0500

    populate repo

---
+++ R/AllClasses.R
@@ -0,0 +1,75 @@
+
+# Dpeak data class
+
+setClass( Class="DpeakData",
+    representation=representation(
+        fragSet="list",
+        
+        PET="logical",
+        fragLenTable="table",
+        Fratio="numeric",
+        aveFragLen="numeric",
+        
+        stackedFragment="list",
+        
+        peakChr="character",
+        peakStart="numeric",
+        peakEnd="numeric",
+        
+        emptyList="character"
+    )
+)
+
+# Dpeak motif class
+
+setClass( Class="DpeakMotif",
+    representation=representation(
+        motif="character",
+        locMotif="list",
+        
+        peakChr="character",
+        peakStart="numeric",
+        peakEnd="numeric"
+    )
+)
+
+# Dpeak fit class
+
+setClass( Class="DpeakFit",
+    representation=representation(
+        fits="list",
+        
+        optFit="list",
+        optMu="list",
+        optPi="list",
+        optPi0="list",
+        optGamma="list",
+        optDelta="list",
+        optSigma="list",
+        bicVec="list",
+        aicVec="list",
+        
+        fragSet="list",
+        PET="logical",
+        fragLenTable="table",
+        Fratio="numeric",
+        aveFragLen="numeric",
+        
+        stackedFragment="list",
+        
+        peakChr="character",
+        peakStart="numeric",
+        peakEnd="numeric",
+        
+        estDeltaSigma="character",
+		nTop="numeric",
+        lbDelta="numeric",
+        lbSigma="numeric",
+        psize="numeric",
+        maxComp="numeric", 
+        pConst="numeric", 
+        iterInit="numeric", 
+        iterMain="numeric", 
+        epsilon="numeric"
+    )
+)
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# dpeak
+dpeak
+=====
+
+dPeak is a statistical framework for the high resolution identification of protein-DNA interaction sites using PET and SET ChIP-Seq and ChIP-exo data. It provides computationally efficient and user friendly interface to process ChIP-seq and ChIP-exo data, implement exploratory analysis, fit dPeak model, and export list of predicted binding sites for downstream analysis. You can track development of mosaics package at http://github.com/dongjunchung/dpeak. To install the development version of dpeak, it's easiest to use the devtools package:
+
+```
+#install.packages("devtools")
+library(devtools)
+install_github("dongjunchung/dpeak")
+```
+
+References
+==========
+
+Chung D, Park D, Myers K, Grass J, Kiley P, Landick R, and Keles S (2013), "dPeak: High resolution identification of transcription factor binding sites from PET and SET ChIP-Seq data," _PLoS Computational Biology_, 9(10): e1003246.
+
+Chung D, Ong I, Grass J, Landick R, Kiley P, and Keles S (2013). "High resolution identification of protein-DNA binding events using ChIP-exo data with dPeak." (in preparation) 
